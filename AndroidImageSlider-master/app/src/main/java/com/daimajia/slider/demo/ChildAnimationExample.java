449e9c3e7b91ba1092f7846bbc66f5f2e2cd1d2b
vvagnersilva <vvagner.silva@gmail.com>
2016-09-23 22:46:11 -0300

    Removendo classe sem utilização.

--- AndroidImageSlider-master/app/src/main/java/com/daimajia/slider/demo/ChildAnimationExample.java
+++ AndroidImageSlider-master/app/src/main/java/com/daimajia/slider/demo/ChildAnimationExample.java
@@ -1,4 +1,4 @@
-package br.ufgd.adipometro.imageSlider;
+package com.daimajia.slider.demo;
 
 import android.util.Log;
 import android.view.View;
@@ -12,24 +12,24 @@ public class ChildAnimationExample implements BaseAnimationInterface {
     @Override
     public void onPrepareCurrentItemLeaveScreen(View current) {
         View descriptionLayout = current.findViewById(com.daimajia.slider.library.R.id.description_layout);
-        if (descriptionLayout != null) {
+        if(descriptionLayout!=null){
             current.findViewById(com.daimajia.slider.library.R.id.description_layout).setVisibility(View.INVISIBLE);
         }
-        Log.e(TAG, "onPrepareCurrentItemLeaveScreen called");
+        Log.e(TAG,"onPrepareCurrentItemLeaveScreen called");
     }
 
     @Override
     public void onPrepareNextItemShowInScreen(View next) {
         View descriptionLayout = next.findViewById(com.daimajia.slider.library.R.id.description_layout);
-        if (descriptionLayout != null) {
+        if(descriptionLayout!=null){
             next.findViewById(com.daimajia.slider.library.R.id.description_layout).setVisibility(View.INVISIBLE);
         }
-        Log.e(TAG, "onPrepareNextItemShowInScreen called");
+        Log.e(TAG,"onPrepareNextItemShowInScreen called");
     }
 
     @Override
     public void onCurrentItemDisappear(View view) {
-        Log.e(TAG, "onCurrentItemDisappear called");
+        Log.e(TAG,"onCurrentItemDisappear called");
     }
 
     @Override
@@ -37,14 +37,14 @@ public class ChildAnimationExample implements BaseAnimationInterface {
 
         View descriptionLayout = view.findViewById(com.daimajia.slider.library.R.id.description_layout);
         //if(descriptionLayout!=null){
-        //view.findViewById(com.daimajia.slider.library.R.id.description_layout).setVisibility(View.VISIBLE);
+            //view.findViewById(com.daimajia.slider.library.R.id.description_layout).setVisibility(View.VISIBLE);
 //            ValueAnimator animator = ObjectAnimator.ofFloat(
 //                    descriptionLayout, "y", -descriptionLayout.getHeight(),
 //                    0).setDuration(500);
 //            animator.start();
 //            new BounceInAnimator().animate(descriptionLayout);
-        //new StandUpAnimator().animate(descriptionLayout);
+            //new StandUpAnimator().animate(descriptionLayout);
         //}
-        Log.e(TAG, "onCurrentItemDisappear called");
+        Log.e(TAG,"onCurrentItemDisappear called");
     }
 }
